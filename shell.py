287c659547b420d9cb415a1c3fc166054e5c47fc
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-08-28 13:09:41 -0500

    [shell] Knocked out some code in the while loop and changed it into a function

--- shell.py
+++ shell.py
@@ -1,5 +1,17 @@
 import core, random
 
+
+def turn(attacker, defender):
+    while True:
+        decision = input('{}:\n what would you like to do?\n [A]ttack\n: '.
+                         format(attacker.name)).upper()
+        if decision == 'A':
+            return core.Moves.normal_attack(attacker, defender)
+            break
+        else:
+            print('Invalid Choice... Please try again\n')
+
+
 print('Welcome to Gladiators'.center(100))
 name = input('What is Gladiator One\'s Name? \n: ').title()
 health = 100
@@ -17,28 +29,13 @@ print('Ready'.center(100))
 print('Fight!!!'.center(100))
 print(core.Battle.__str__(glad_1, glad_2))
 decision = ''
-while (glad_1.is_dead() != True) or (glad_2.is_dead() != True):
-    while True:
-        decision = input('{}:\n what would you like to do?\n [A]ttack\n: '.
-                         format(glad_1.name)).upper()
-        if decision == 'A':
-            core.Moves.normal_attack(glad_1, glad_2)
-            break
-        else:
-            print('Invalid Choice... Please try again\n')
+while True:
+    turn(glad_1, glad_2)
     print(core.Battle.__str__(glad_1, glad_2))
     if (glad_2.is_dead() == True):
         print('Game Over!!! {} WINS!!!'.format(glad_1.name))
         quit()
-    while True:
-        decision = input('{}:\n what would you like to do?\n [A]ttack\n: '.
-                         format(glad_2.name)).upper()
-        if decision == 'A':
-            core.Moves.normal_attack(glad_2, glad_1)
-            break
-        else:
-            print('Invalid Choice... Please try again\n')
-
+    turn(glad_2, glad_1)
     print(core.Battle.__str__(glad_1, glad_2))
     if (glad_2.is_dead() == True):
         print('Game Over!!! {} WINS!!!'.format(glad_2.name))
